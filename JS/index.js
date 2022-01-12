be15a7fcc85608c9c4c01a295ff6a3410f5bb3c1
carlos <carlosalvarezsanchez17@gmail.com>
2022-01-12 11:08:49 +0100

    Normalizando 1.1

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,9 @@
 //Creacion da base de datos
 window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
 
-window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction || {READ_WRITE: "readwrite"}; // This line should only be needed if it is needed to support the object's constants for older browsers
-window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
 
 if(!window.indexedDB){
     console.log("El buscador no puede cargar el IndexedDB");
 }
+
+/*IIFE*/
