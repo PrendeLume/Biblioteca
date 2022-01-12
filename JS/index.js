7cc861d5472f2d3fee6884abe3d34feaffea373f
carlos <carlosalvarezsanchez17@gmail.com>
2022-01-12 11:10:09 +0100

    Normalizando 1.2

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,6 @@
 //Creacion da base de datos
 window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
 
-
 if(!window.indexedDB){
     console.log("El buscador no puede cargar el IndexedDB");
 }
